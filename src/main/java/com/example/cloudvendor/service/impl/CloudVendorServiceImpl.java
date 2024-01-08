438ca6277f26e31963dfdde1aa4fa45b5aaef8db
alvinotuya2 <teetinormm@gmail.com>
2024-01-08 08:14:48 +0300

    service implementation

---
+++ src/main/java/com/example/cloudvendor/service/impl/CloudVendorServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.cloudvendor.service.impl;
+
+import com.example.cloudvendor.model.CloudVendor;
+import com.example.cloudvendor.repository.CloudVendorRepository;
+import com.example.cloudvendor.service.CloudVendorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CloudVendorServiceImpl implements CloudVendorService {
+    CloudVendorRepository cloudVendorRepository;
+    public CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Updated success";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        cloudVendorRepository.deleteById(cloudVendorId);
+        return "Success";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+        return cloudVendorRepository.findById(cloudVendorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendor() {
+
+        return cloudVendorRepository.findAll();
+    }
+}
